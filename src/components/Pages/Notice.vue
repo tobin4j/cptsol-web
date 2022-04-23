1b03edc884bbc859d73018c6efd9065ed166cc44
2442546040@qq.com <12>
2022-04-23 22:14:49 +0800

    切换导航菜单

---
+++ src/components/Pages/Notice.vue
@@ -0,0 +1,25 @@
+<template>
+  <!-- 通知公告 -->
+  通知公告
+</template>
+
+<script>
+import Banner from '../Banner.vue'
+import Main from '../Main.vue'
+export default {
+  name: 'App',
+   data () {
+    return {
+       
+    }
+   },
+  components: {
+    Banner,
+    Main
+  }
+}
+</script>
+
+<style scoped>
+ 
+</style>
---
+++ src/components/Pages/TestSys.vue
@@ -0,0 +1,25 @@
+<template>
+  <!-- 考试系统 -->
+  考试系统
+</template>
+
+<script>
+import Banner from '../Banner.vue'
+import Main from '../Main.vue'
+export default {
+  name: 'App',
+   data () {
+    return {
+       
+    }
+   },
+  components: {
+    Banner,
+    Main
+  }
+}
+</script>
+
+<style scoped>
+ 
+</style>
